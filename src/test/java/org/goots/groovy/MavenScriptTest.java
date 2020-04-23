c01b027bcb128c82d88a9358c05f3028367fbfb0
Nick Cross <ncross@redhat.com>
2020-04-23 13:48:32 +0100

    NCL-5708 Add groovy example demonstrating adding a module

--- src/test/java/org/goots/groovy/MavenScriptTest.java
+++ src/test/java/org/goots/groovy/MavenScriptTest.java
@@ -10,14 +10,17 @@ import org.commonjava.maven.ext.common.model.Project;
 import org.commonjava.maven.ext.core.ManipulationManager;
 import org.commonjava.maven.ext.core.ManipulationSession;
 import org.commonjava.maven.ext.core.fixture.TestUtils;
+import org.commonjava.maven.ext.core.fixture.TestUtils.SMContainer;
 import org.commonjava.maven.ext.core.groovy.BaseScript;
 import org.commonjava.maven.ext.core.impl.InitialGroovyManipulator;
 import org.commonjava.maven.ext.io.ModelIO;
 import org.commonjava.maven.ext.io.PomIO;
 import org.commonjava.maven.ext.io.resolver.GalleyAPIWrapper;
 import org.commonjava.maven.ext.io.resolver.GalleyInfrastructure;
+import org.eclipse.jgit.api.Git;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.rules.TemporaryFolder;
 
@@ -26,6 +29,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -38,7 +42,43 @@ public class MavenScriptTest
     public TemporaryFolder temp = new TemporaryFolder();
 
     @Rule
-    public final SystemOutRule systemRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
+
+    @Rule
+    public final SystemErrRule systemErrRule = new SystemErrRule().enableLog().muteForSuccessfulTests();
+
+    @Test
+    public void testQuarkusGroovyAnnotation() throws Exception
+    {
+        final File preGroovy = GroovyLoader.loadGroovy( "quarkusPlatformPre.groovy" );
+        final File quarkusFolder = temp.newFolder();
+        final File mvnRepo = temp.newFolder();
+        final File rootPom = new File( quarkusFolder.getCanonicalFile(), "pom.xml");
+
+        Git.cloneRepository()
+           .setURI("https://github.com/quarkusio/quarkus-platform.git")
+           .setDirectory(quarkusFolder)
+           .setBranch( "refs/tags/1.3.1.Final")
+           .call();
+        System.out.println ("Cloned Quarkus to " + quarkusFolder);
+
+        // While it would be nice to be able to just do "new Cli().run(....)" due to project setup complexities (e.g. running
+        // inside Maven, Gradle dependencies etc) we get Google Inject CreationErrors. Therefore use the following block to
+        // replicate the CLI
+        Properties prop = new Properties();
+        prop.setProperty( "groovyScripts", "file://" + preGroovy.getAbsolutePath() );
+        prop.setProperty( "maven.repo.local", mvnRepo.toString() );
+        SMContainer smc = TestUtils.createSessionAndManager( prop );
+        smc.getRequest().setPom( rootPom );
+        smc.getManager().scanAndApply( smc.getSession() );
+
+        List<Project> result = new PomIO().parseProject( rootPom );
+        assertTrue( new File(quarkusFolder, "bom/product-bom").exists() );
+        assertEquals( 5, result.get( 0 ).getModel().getModules().size() );
+        assertEquals( result.stream().filter( p -> p.getArtifactId().equals( "quarkus-product-bom" ) )
+                            .map( pr -> pr.getModel().getDependencyManagement().getDependencies().size() ).findFirst().orElse( 0 ),
+                      Integer.valueOf( 445 ) );
+    }
 
     @Test
     public void testGroovyAnnotation() throws Exception {
@@ -58,7 +98,7 @@ public class MavenScriptTest
         gm.init(ms);
         TestUtils.executeMethod(gm, "applyGroovyScript", new Class[]{List.class, Project.class, File.class},
                 new Object[]{projects, root, groovy});
-        assertTrue(systemRule.getLog().contains("BASESCRIPT"));
+        assertTrue( systemOutRule.getLog().contains( "BASESCRIPT") );
 
         List<String> result = projects.get(0).getModel().getDependencies().stream().
                 filter(d -> d.getGroupId().equals("org.apache.maven") && d.getArtifactId().equals("maven-core")).
